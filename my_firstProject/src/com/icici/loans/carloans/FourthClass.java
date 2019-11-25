284457c7cd4a4977638d37ffbb7fff9418b53c64
mbasrai <mbasrai68@gmail.com>
2019-11-24 21:10:51 -0500

    new change

---
+++ my_firstProject/src/com/icici/loans/carloans/FourthClass.java
@@ -0,0 +1,24 @@
+package com.icici.loans.carloans;
+
+public class FourthClass 
+
+{
+	int a = 10,b = 20,c ;
+	
+	public void add ()
+	{
+		
+		c= a+b;
+		System.out.println(" I am addition of A & B :" + c) ;
+		//c=a-b;
+	}
+	
+
+	public static void main(String[] args) 
+	
+	{
+		
+
+	}
+
+}
