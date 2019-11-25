6d787dff3e3a4069cf6873fbb4423dcee41f04a8
mbasrai <mbasrai68@gmail.com>
2019-11-24 21:24:58 -0500

    this is a practice

---
+++ my_firstProject/src/com/icici/loans/carloans/FourthClass.java
@@ -0,0 +1,23 @@
+package com.icici.loans.carloans;
+
+public class FourthClass 
+
+{
+    int a = 10,b = 20,d ;
+	
+	public void add ()
+	{
+		
+		d= a+b;
+		System.out.println(" I am addition of A & B :" + d) ;
+		//c=a-b;
+	}
+
+	public static void main(String[] args) 
+	
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
